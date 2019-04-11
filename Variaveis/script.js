ad7fd8dfa851b54718821832c7c0f902f5f12fea
José Matela <jmatelaneto@gmail.com>
2019-04-11 14:53:33 -0300

    Variables module finished

---
+++ Variaveis/script.js
@@ -0,0 +1,50 @@
+// Study
+
+/*
+var name = 'Jose'
+console.log(name)
+
+let age = 19
+console.log(age)
+
+const doingCollege = true;
+console.log(doingCollege)
+
+console.log(name, age, doingCollege)
+
+var price = 20
+var totalBought = 5
+var totalPrice = price * totalBought
+console.log(totalPrice)
+
+var lastName = 'Matela'
+var city = 'Fortaleza'
+console.log(name, lastName, age, city)
+
+var withoutDefining
+console.log(withoutDefining)
+
+var team = 'Ceara'
+console.log(team)
+team = 'Ceara FOREVER'
+console.log(team)
+*/
+
+// Exercices
+
+// Declare a variable with your name
+var name = 'Matela'
+
+// Declare a variable with your age
+var age = 19
+
+// Declare a variable with your favorite food and do not assign value
+var favotireFood
+
+// Assign value to your favorite food
+favotireFood = 'Beef'
+
+// Declare 5 different valueless variables
+var oloco, bixo, brincadeira, em, meu
+
+console.log(name, age, favotireFood, oloco, bixo, brincadeira, em, meu)
