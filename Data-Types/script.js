c0688b0b8f2ca56f7eea139b25af77096a4da44e
José Matela <jmatelaneto@gmail.com>
2019-04-11 15:29:09 -0300

    Data Types module finished

---
+++ Data-Types/script.js
@@ -0,0 +1,66 @@
+// Study
+
+/*
+var name = 'Matela' // String
+var age = 19 // Number
+var doingCollege = true // Boolean
+var team // Undefined
+var food = null // Null
+var symbol = Symbol() // Symbol
+var newObject = {} // Object
+
+console.log('name variable, type is', typeof name)
+console.log('age variable, type is', typeof age)
+console.log('doingCollege variable, type is', typeof doingCollege)
+console.log('team variable, type is', typeof team)
+console.log('food variable, type is', typeof food)
+console.log('symbol variable, type is', typeof symbol)
+console.log('newObject variable, type is', typeof newObject)
+
+// Strings
+// Concat string
+
+var name = 'José'
+var lastName = 'Matela'
+var completeName = name + ' ' + lastName
+console.log(completeName)
+
+// Concat number with string
+
+var goals = 1000
+var phrase = completeName + ' make ' + goals + ' goals'
+console.log(phrase)
+console.log(typeof phrase)
+
+// Template String
+
+var newPhrase =  `${completeName} make ${goals * 2} goals`
+console.log(newPhrase)
+*/
+
+// Exercices
+
+// Declare a variable containing a string
+var someVariable = 'Hi there'
+console.log(someVariable)
+
+// Declare a variable containing a number inside a string
+var numberString = '777'
+console.log(numberString)
+
+// Declare a variabel containing your age
+var age = 19
+console.log(age)
+
+// State two variables, one with your name and one with your last name and add the same
+var name = 'José'
+var lastName = 'Matela'
+var completeName = `${name} ${lastName}`
+console.log(completeName)
+
+// Put this phrase in a variable: It's time
+var phrase = `It's time`
+console.log(phrase)
+
+// Check the type of variable that contains your name
+console.log(typeof completeName)
